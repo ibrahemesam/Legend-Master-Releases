0d1aa98fc72175115791e5447b0cded3ed435aee
ibrahem <iimosa7777@gmail.com>
2022-02-12 02:10:03 +0200

    (:

--- Legend-Master-unix-install.py
+++ Legend-Master-unix-install.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 #NOTE: python3 is already in all linux distros, so: no need for shell (:
-
+p = print
 from subprocess import Popen as popen
 import sys, os
 from random import randint
 try: import requests
-except: popen([sys.executable, '-m', 'pip', 'install', 'requests']).wait()
+except:
+    p('installing missing pkg: python3-requests')
+    popen([sys.executable, '-m', 'pip', 'install', 'requests']).wait()
 
 # create an empty folder
+p('creating app_dir')
 app_dir = "Legend-Master-unix"
 if os.path.exists(app_dir):
     while True:
@@ -18,21 +21,27 @@ else: print("app_dir is ok")
 os.mkdir(app_dir)
 
 #wget and install miniconda3
+p('wget miniconda3')
 popen(['wget', 'https://github.com/ibrahemesam/Legend-Master-Releases/releases/download/Python-Runtime/Miniconda3-py3.9.5-Linux-x86_64.sh']).wait()
+p('installing miniconda3')
 popen(['chmod', '+x', './Miniconda3-py3.9.5-Linux-x86_64.sh']).wait()
 popen(['bash', './Miniconda3-py3.9.5-Linux-x86_64.sh', '-b', '-p', f'{app_dir}/lenv-64']).wait()
 
 ##install_script.py##
 #get unix64.zip
+p('wget unix64.zip')
 popen(['wget', 'https://github.com/ibrahemesam/Legend-Master-Releases/releases/download/latest/unix64.zip']).wait()
 #unzip it to app_dir/unix64
 import zipfile
 with zipfile.ZipFile('unix64.zip', 'r') as zip_ref:
+    p('unzipping unix64.zip')
     zip_ref.extractall(f"{app_dir}/unix64")
 #run the app (popen new session)
+p('running app')
 popen(['chmod', '+x', f'{app_dir}/unix64/run.sh']).wait()
 popen(['bash', os.path.abspath(f'{app_dir}/unix64/run.sh')], start_new_session=True, cwd=f'{app_dir}/unix64')
 #clean: rm Miniconda3*sh && rm unix64.zip && rm this .py file
+p('cleaning this miss')
 os.remove("Miniconda3-py3.9.5-Linux-x86_64.sh")
 os.remove("unix64.zip")
 os.remove(sys.argv[0])
--- Legend-Master-win-install.py
+++ Legend-Master-win-install.py
@@ -2,6 +2,7 @@
 #a script to auto-download the app
 from subprocess import Popen as popen
 import sys, os, ctypes
+p = print
 
 def get_windows_bit_length():
     size = ctypes.sizeof(ctypes.c_voidp)
@@ -12,16 +13,20 @@ def get_windows_bit_length():
 ##install_script.py##
 bits = get_windows_bit_length()
 #get win*.zip
+p(f'wget win{bits}.zip')
 popen(['powershell', '-c', f'''"Invoke-WebRequest -Uri 'https://github.com/ibrahemesam/Legend-Master-Releases/releases/download/latest/win{bits}.zip' -OutFile win{bits}.zip"''']).wait()
 
 #unzip it to app_dir/win{bits}
 import zipfile
 with zipfile.ZipFile(f'win{bits}.zip', 'r') as zip_ref:
+    p(f'unzipping win{bits}.zip')
     zip_ref.extractall(f"{app_dir}/win{bits}")
 #run the app (popen new session)
+p('running app')
 popen([f'{app_dir}/win{bits}/run.exe'], start_new_session=True, cwd=f'{app_dir}/win{bits}')
 
-#clean: rm Miniconda3*sh && rm unix64.zip && rm this .py file
+#clean: rm wenv-*.zip && rm win*.zip.zip && rm this .py file
 def rm(f): os.remove(f)
+p('cleaning this miss')
 [rm(f) for f in ['unzip.exe', f'wenv-{bits}.zip', sys.argv[0], 'Legend-Master-win-install.bat', f'win{bits}.zip']]
 
